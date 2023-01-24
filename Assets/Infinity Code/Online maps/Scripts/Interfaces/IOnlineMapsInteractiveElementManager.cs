9e837dffeeb71801185f0f36fbe6f17bea1891b7
meepermad <atem.akuei@gmail.com>
2023-01-24 10:06:18 -0600

    first commit

--- Assets/Infinity Code/Online maps/Scripts/Interfaces/IOnlineMapsInteractiveElementManager.cs
+++
@@ -1,8 +0,0 @@
-/*         INFINITY CODE         */
-/*   https://infinity-code.com   */
-
-public interface IOnlineMapsInteractiveElementManager
-{
-    OnlineMaps map { get; }
-    int IndexOf(IOnlineMapsInteractiveElement element);
-}
